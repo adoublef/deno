68627102599894a7d9c7af6d601b9e9d4e709992
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-26 17:31:59 +0000

    update to use hono@v3.9.2

--- jsx/control_flow/deps.ts
+++ jsx/control_flow/deps.ts
@@ -1,3 +1,4 @@
 export { jsx, JSXNode } from "https://deno.land/x/hono@v3.9.2/jsx/index.ts";
+export type { FC } from "https://deno.land/x/hono@v3.9.2/jsx/index.ts";
 export { html } from "https://deno.land/x/hono@v3.9.2/helper/html/index.ts";
 export type { HtmlEscapedString } from "https://deno.land/x/hono@v3.9.2/utils/html.ts";
