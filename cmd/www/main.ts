ea12139c2e61d9ac52cbf7eafa78c3c0aa100415
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-26 14:41:29 +0000

    remove cmd

--- cmd/www/main.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "../../serve.ts";
-import { Hono } from "~/deps.ts";
-
-if (import.meta.main) {
-    const app = new Hono()
-        .get("/", (_) => new Response("ok"));
-
-    await serve(app);
-}
