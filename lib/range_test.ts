f84c1a55fe494cd391c792f2eaafd5656214314c
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-26 17:54:16 +0000

    remove blot

--- lib/range_test.ts
+++
@@ -1,9 +0,0 @@
-import { assertArrayIncludes } from "$std/assert/mod.ts";
-import { range } from "./range.ts";
-
-Deno.test("range", async (t) => {
-    await t.step("should spread a range generator", () => {
-        const rs = [...range(0, 3)];
-        assertArrayIncludes(rs, [0, 1, 2]);
-    });
-});
