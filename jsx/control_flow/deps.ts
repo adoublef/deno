aa66680632816e6e276069cc123aaedaeb7bf686
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-26 19:20:23 +0000

    format

--- jsx/control_flow/deps.ts
+++ jsx/control_flow/deps.ts
@@ -1,4 +1,8 @@
-export { jsx,Fragment,  JSXNode } from "https://deno.land/x/hono@v3.10.2/jsx/index.ts";
+export {
+    Fragment,
+    jsx,
+    JSXNode,
+} from "https://deno.land/x/hono@v3.10.2/jsx/index.ts";
 export type { FC } from "https://deno.land/x/hono@v3.10.2/jsx/index.ts";
 export { html } from "https://deno.land/x/hono@v3.10.2/helper/html/index.ts";
 export type { HtmlEscapedString } from "https://deno.land/x/hono@v3.10.2/utils/html.ts";
--- jsx/control_flow/for.tsx
+++ jsx/control_flow/for.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 /** @jsxFrag Fragment */
-import { jsx, Fragment } from "./deps.ts";
-import { html, HtmlEscapedString } from "./deps.ts";
+import { Fragment, jsx } from "./deps.ts";
+import { HtmlEscapedString } from "./deps.ts";
 
 /** [TODO](https://www.solidjs.com/docs/latest/api#for) */
 export const For = <T,>({
