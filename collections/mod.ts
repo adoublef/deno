661d084457f137e059ca88508700a0e214fe08e4
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-11-26 17:58:39 +0000

    fmt

--- collections/mod.ts
+++ collections/mod.ts
@@ -1,1 +1,1 @@
-export * from "./range.ts"
+export * from "./range.ts";
--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-export * from "./collections/mod.ts"
+export * from "./collections/mod.ts";
 export * from "./jsx/mod.ts";
 export * from "./net/mod.ts";
 // export * from "./database/mod.ts" // unsure if this will break if I include libsql
